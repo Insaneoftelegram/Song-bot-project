f0b90d5bab65a2ed9e001a3f8320b2f723016c55
Insaneoftelegram <104971406+Insaneoftelegram@users.noreply.github.com>
2023-06-06 00:32:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+from pyrogram import Client, filters
+
+
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+
+
+INSANE = Client(
+    name="insane test bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+
+@INSANE.on_message(filters.command("start")
+async def start_cmd(Client, message):
+    await message.reply_text("Hello......!!!")
+ 
+
+
+
+print("Bot started ")
+INSANE.run()
 
