b9326ce83e5577bb68afb4d664ae67d55b62e998
Insaneoftelegram <104971406+Insaneoftelegram@users.noreply.github.com>
2023-06-06 13:11:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
 from pyrogram import Client, filters
 
-
-import youtube_dl
-from youtube_search import YoutubeSearch
-import requests
-
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-import os
-from config import Config
-
-import pyrogram
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-
 API_ID = "5642193"
 API_HASH = "c28fc9ac88530587236175da89184d75"
 BOT_TOKEN = "6297344590:AAFbBHK9PioaIS0sZnH0jR4a4Sp7859Rt_4"
