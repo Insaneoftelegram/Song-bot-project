d53bbd7e4e00ff37208efae10dc270fbd5cda15e
Insaneoftelegram <104971406+Insaneoftelegram@users.noreply.github.com>
2023-06-06 14:49:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ INSANE = Client(
 )
 
 START_BUTTONS =[[
-    InlineKeyboardButton("ADD ME YOUR GROUP", url="https://t.me/Resso_offical_bot?startgroup=true")
+    InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ", url="https://t.me/Resso_offical_bot?startgroup=true")
     ],[
-    InlineKeyboardButton("Owner", url="t.me/insane_of_telegram"),
-    InlineKeyboardButton("Source", url="t.me/INSANEX3")
+    InlineKeyboardButton("ᴄʜᴀɴɴᴇʟ", url="t.me/INSANEX3"),
+    InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/INSANEX3_SUPPORT")
     ]]
 
 @INSANE.on_message(filters.command("start"))
