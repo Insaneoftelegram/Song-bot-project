4b0389a69e8457cc499ea0d460a186fafe641ba4
Insaneoftelegram <104971406+Insaneoftelegram@users.noreply.github.com>
2023-06-06 02:53:27 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, filters
 
 
-API_ID = ""
-API_HASH = ""
-BOT_TOKEN = ""
+API_ID = "5642193"
+API_HASH = "c28fc9ac88530587236175da89184d75"
+BOT_TOKEN = "6297344590:AAFbBHK9PioaIS0sZnH0jR4a4Sp7859Rt_4"
 
 
 INSANE = Client(
