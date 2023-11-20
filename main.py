f7c513344f78318ed1850bc8ec93e8804ec76903
Insaneoftelegram <104971406+Insaneoftelegram@users.noreply.github.com>
2023-11-20 15:47:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,53 @@
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import os
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from youtube_dl import YoutubeDL
 
-API_ID = "5642193"
-API_HASH = "c28fc9ac88530587236175da89184d75"
-BOT_TOKEN = "6088570842:AAEP-Stzkhzj53aijcDOeDbhs-hZo9qjeLU"
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
 
+logger = logging.getLogger(__name__)
 
-INSANE = Client(
-    name="insane test bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
+# Define the start command handler
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I am a song download bot. Just send me the name of the song you want to download.")
 
-START_BUTTONS =[[
-    InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ", url="https://t.me/Resso_offical_bot?startgroup=true")
-    ],[
-    InlineKeyboardButton("ᴄʜᴀɴɴᴇʟ", url="t.me/INSANEX3"),
-    InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/RESSO_SUPPORT")
-    ]]
+# Define the message handler
+def message_handler(update, context):
+    song_name = update.message.text
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(f"ytsearch:{song_name}", download=False)
+        url = info['entries'][0]['formats'][0]['url']
+        context.bot.send_audio(chat_id=update.effective_chat.id, audio=url)
 
-@INSANE.on_message(filters.command("start"))
-async def start_cmd(Client, message):
-    await message.reply_photo(
-        photo="https://telegra.ph/file/da545a93169c6e91d4c98.jpg",
-        caption="ʜᴇʏ, \n \n \n ɪ'ᴍ ᴀ ᴛᴇʟᴇɢʀᴀᴍ sᴛʀᴇᴀᴍɪɴɢ ʙᴏᴛ ᴡɪᴛʜ sᴏᴍᴇ ᴜsᴇғᴜʟ ғᴇᴀᴛᴜʀᴇ. sᴜᴘᴘᴏʀᴛɪɴɢ ᴘʟᴀᴛғᴏʀᴍs ʟɪᴋᴇ ʏᴏᴜᴛᴜʙᴇ,ʀᴇssᴏ....ᴇᴛᴄ \n \n A ᴘᴏᴡᴇғᴜʟ ᴍᴜsɪᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ ᴡɪᴛʜ sᴏᴍᴇ ᴀᴡᴇsᴏᴍᴇ ᴀɴᴅ ᴜsᴇғᴜʟ ғᴇᴀᴛᴜʀᴇs.\n maintenance work loading..........",
-        reply_markup =InlineKeyboardMarkup (START_BUTTONS)
-  
-    )
-    
-INSANE_BUTTONS =[[
-    InlineKeyboardButton("Owner", url="https://t.me/insanex3")
-    ]]
-    
-@INSANE.on_message(filters.command("owner"))
-async def owner_cmd(client, message):
-    await message.reply_photo(
-        photo="https://telegra.ph/file/a42e0a1e09e4ca442fe3b.jpg",
-        reply_markup =InlineKeyboardMarkup (INSANE_BUTTONS)
-    )
+# Define the main function
+def main():
+    # Create the updater and pass in your bot token
+    updater = Updater(token=os.environ.get('5021398357:AAE62C4x-jptJ3S2gLpuHvrXru6xM5yIAa'), use_context=True)
 
-    
- 
-print("INSANE Bot started ")
-INSANE.run()
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
 
+    # Add the start command handler
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    # Add the message handler
+    dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
+
+    # Start the bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
